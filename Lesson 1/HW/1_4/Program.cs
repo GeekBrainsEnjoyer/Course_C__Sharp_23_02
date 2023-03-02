a6e702f623d37effdedb3f3c0fea9ef8bd8ecef0
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-02 05:38:31 +0300

    HW 1_4

---
+++ Lesson 1/HW/1_4/Program.cs
@@ -0,0 +1,16 @@
+﻿
+//Задача 4: Напишите программу, которая на вход
+//принимает число (N), а на выходе показывает все
+//чётные числа от 1 до N.
+
+Console.WriteLine("Введите число");
+int num1 = int.Parse(Console.ReadLine()!);
+ 
+int count = 2;
+ Console.WriteLine("Четные числа до " + num1);
+while(count <= num1){
+
+    Console.WriteLine($"{count} ");
+    count = count + 2;
+
+}

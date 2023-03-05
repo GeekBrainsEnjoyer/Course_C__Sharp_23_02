5e2369987f5d1fec0f52fc039977955aaedd89c7
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-05 08:11:26 +0300

    hw task 2_4

---
+++ Lesson 2/HW/2_4/Program.cs
@@ -0,0 +1,24 @@
+﻿// Задача 4: Напишите программу, которая принимает на
+// вход цифру, обозначающую день недели, и проверяет,
+// является ли этот день выходным.
+
+Console.WriteLine("Введите цифру, обозначающую день недели");
+int num = int.Parse(Console.ReadLine()!);
+
+void holidays(int num)
+{
+    if(num < 1 | num > 7)
+    {
+        Console.WriteLine("нет такого дня недели");
+    }
+    else if( num == 6 | num ==7)
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+
+holidays(num);

9f60673e830ca58d34727cf9188ed1ba19a7abb8
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-05 07:33:30 +0300

    hw task 2_1

---
+++ Lesson 2/HW/2_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите программу, которая принимает на
+// вход трёхзначное число и на выходе показывает вторую
+// цифру этого числа.
+
+Console.WriteLine("Введите трехзначное число");
+int num = int.Parse(Console.ReadLine()!);
+
+void secondNumber(int num)
+{
+    if(num < 100 | num > 999)
+    {
+        Console.WriteLine("Число должно быть трехзначным");
+    }
+    else
+    {
+    int operation1 = num / 10;
+    int operation2 = operation1 % 10;
+    Console.WriteLine(operation2);
+    }
+}
+
+secondNumber(num);

c5b8287ad753bc7cfec203632e153b8079743200
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-07 01:34:50 +0300

    HW task 3_2

---
+++ Lesson 3/HW/3_2/Program.cs
@@ -0,0 +1,15 @@
+﻿// 2. Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 3D пространстве.
+
+double dis(double x1, double y1,  double z1, double x2, double y2,  double z2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2) + Math.Pow(z2 - z1,2));
+}
+Console.WriteLine("Введите координаты");
+int x1 = int.Parse(Console.ReadLine()!);
+int y1 = int.Parse(Console.ReadLine()!);
+int z1 = int.Parse(Console.ReadLine()!);
+int x2 = int.Parse(Console.ReadLine()!);
+int y2 = int.Parse(Console.ReadLine()!);
+int z2 = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine(dis(x1, y1, z1, x2, y2, z2));

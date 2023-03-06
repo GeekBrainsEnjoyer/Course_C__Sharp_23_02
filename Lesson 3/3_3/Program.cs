3a2b35f038b8e21d4b1f12e535fc9fbaa2382404
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-06 09:28:55 +0300

    task 3_3

---
+++ Lesson 3/3_3/Program.cs
@@ -0,0 +1,10 @@
+﻿// 3. Таблица квадратов от 1 до N
+
+void sqaureTab(int num)
+{
+    for (int i = 1; i < num; i++)
+    {
+        Console.WriteLine(Math.Pow(i,2));
+    }
+}
+sqaureTab(int.Parse(Console.ReadLine()!));

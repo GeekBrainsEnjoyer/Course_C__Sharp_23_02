0c0633a8671b4c749a1549dd78a5df07bca4f9fa
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-06 09:05:26 +0300

    task 3_2

---
+++ Lesson 3/3_2/Program.cs
@@ -0,0 +1,7 @@
+﻿// 2. Растояние между точками в 2Д пространстве
+
+double dis(double x1, double y1, double x2, double y2)
+{
+    return Math.Sqrt(Math.Pow(x2 - x1,2) + Math.Pow(y2 - y1,2));
+}
+Console.WriteLine(dis(3, 6, 2, 1));

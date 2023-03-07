f528bd1203b7b2134358003e3940de8068ca45f2
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-07 13:17:28 +0300

    HW task 4_2

---
+++ Lesson 4/HW/4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая принимает на
+// вход число и выдаёт сумму цифр в числе.
+
+int sumInNum(int num)
+{
+    int mod = 0;
+    for (int i = 0; num > 0; i++)
+    {
+        mod = mod + num % 10;
+        num = num / 10;
+    }
+    return mod;
+}
+
+Console.WriteLine("Введите число");
+int result = sumInNum(int.Parse(Console.ReadLine()!));
+Console.WriteLine(result);

10e7023206dced75b4bd8dcee1ccd00e1e7ab97d
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-07 06:06:17 +0300

    task 4_1

---
+++ Lesson 4/4_1/Program.cs
@@ -0,0 +1,17 @@
+﻿// // Задача 1: Напишите программу, которая принимает
+// на вход число и выдаёт количество цифр в числе.
+
+int findCountNum(int num)
+{
+    int count = 1;
+    while(num > 9)
+    {
+        num = num / 10;
+        count++;
+    }
+    return count;
+}
+
+int A = int.Parse(Console.ReadLine()!);
+int result = findCountNum(A);
+Console.WriteLine(result);

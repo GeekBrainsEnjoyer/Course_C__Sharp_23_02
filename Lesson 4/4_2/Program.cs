7bf8809cf8a322abcab4a1724d7b4e34d2ee83a5
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-07 06:14:13 +0300

    task 4_2

---
+++ Lesson 4/4_2/Program.cs
@@ -0,0 +1,17 @@
+﻿// Задача 2: Напишите программу, которая
+// принимает на вход число N и выдаёт
+// произведение чисел от 1 до N.
+
+int mutiplyNum(int num)
+{
+    int all_multi = 1;
+    for (int i = 1; i <= num; i++)
+        all_multi = all_multi * i;
+    
+    return all_multi;
+}
+
+int N = int.Parse(Console.ReadLine()!);
+int result = mutiplyNum(N);
+Console.WriteLine(result);
+

87cb9369402e4055371a5fc2e235faf6a8d2c043
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-07 12:55:04 +0300

    HW task 4_1

---
+++ Lesson 4/HW/4_1/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 1: Напишите цикл, который принимает на вход
+// два числа (A и B) и возводит число A в натуральную
+// степень B.
+
+int expanentiation(int num, int exp)
+{
+    int n = 1;
+    for (int i = 0; i < exp; i++)
+    {
+        n = n * num;
+    }
+    return n;
+}
+
+Console.WriteLine("Введите число А");
+int A = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+Console.WriteLine("Введите степень В");
+int B = int.Parse(Console.ReadLine()!);
+Console.WriteLine();
+int result = expanentiation(A, B);
+Console.WriteLine(result);

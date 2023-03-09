275d892cd850826c90c65f26503b0ea6f459d296
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-09 10:56:25 +0300

    task 5_2 need check

---
+++ Lesson 5/5_2/Program.cs
@@ -0,0 +1,50 @@
+﻿// Задача 2: Задайте массив. Напишите программу, которая
+// определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// 3; массив [6, 7, 19, 345, 3] -> да
+
+void fillArray(int[] collection, int botLine, int upLine)
+{
+    int length = collection.Length;
+    int index = 0;
+    while (index < length){
+        collection[index] = new Random().Next(botLine, upLine);
+        index++;
+    }
+}
+
+void printArray(int[] col)
+{
+    int count = col.Length;
+    int position = 0;
+    while(position < count){
+        Console.WriteLine(col[position]);
+        position++;
+    }
+    Console.WriteLine();
+}
+
+void findNum(int[] mass, int num)
+{
+    for (int i = 0; i < mass.Length; i++)
+    {
+        if(mass[i] == num)
+            Console.WriteLine("Yes");
+            return;
+    }
+    Console.WriteLine("no");
+}
+
+Console.WriteLine("Введите размер массива");
+int[] array = new int[int.Parse(Console.ReadLine()!)];
+
+Console.WriteLine("Введите нижнюю границу массива");
+int bot = int.Parse(Console.ReadLine()!);
+
+Console.WriteLine("Введите верхнюю гранцу массива");
+int up = int.Parse(Console.ReadLine()!);
+
+fillArray(array, bot, up);
+printArray(array);
+int num = int.Parse(Console.ReadLine()!);
+findNum(array, num);

b560bda91dee28e2f4c4871be6a4af58654c211d
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-17 13:05:49 +0300

    task 7_0 2Darray

---
+++ Lesson 7/7_0/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 1: Задайте двумерный массив размером m×n,
+// заполненный случайными целыми числами.
+// m = 3, n = 4.
+// 1 4 8 19
+// 5 -2 33 -2
+// 77 3 8 1
+
+void print2DArray(int[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            System.Console.Write($"{array[i, j]} ");
+
+        System.Console.WriteLine();
+    }
+}
+
+int[,] fill2DArray(int rows, int columns, int from, int to)
+{
+    int[,] array = new int[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+           array[i, j] = new Random().Next(from, to);
+    
+    return array;
+}
+
+System.Console.WriteLine("Enter rows and columns");
+int ro = int.Parse(Console.ReadLine()!);
+int cl = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Enter values from-to");
+int fr = int.Parse(Console.ReadLine()!);
+int to = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine();
+
+int[,] matrix = fill2DArray(ro, cl, fr, to);
+print2DArray(matrix);
+System.Console.WriteLine();

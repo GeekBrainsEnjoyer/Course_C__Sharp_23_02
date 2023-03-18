46a3897aad962a605efa28731b132181e20d8085
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-18 12:26:46 +0300

    hw task 7_1

---
+++ Lesson 7/HW/7_1/Program.cs
@@ -0,0 +1,43 @@
+﻿// Задача 47.Задайте двумерный массив размером m×n, заполненный случайными вещественными числами.
+// m = 3, n = 4.
+// 0,5 7 -2 -0,2
+// 1 -3,3 8 -9,9
+// 8 7,8 -7,1 9
+
+void printDouble2DArray(double[,] array)
+{
+    int rows = array.GetLength(0);
+    int columns = array.GetLength(1);
+
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+            System.Console.Write($"{array[i, j]}    ");
+        System.Console.WriteLine();
+    }
+}
+
+double[,] fillDouble2DArray(int rows, int columns, int from, int to)
+{
+    double[,] array = new double[rows, columns];
+
+    for (int i = 0; i < rows; i++)
+        for (int j = 0; j < columns; j++)
+        {
+            double value = new Random().NextDouble() * (from - to) + from;
+            array[i, j] = Math.Round(value, 2);
+        }
+    return array;
+}
+
+System.Console.WriteLine("Enter rows and columns");
+int row = int.Parse(Console.ReadLine()!);
+int col = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine("Enter values from-to");
+int from = int.Parse(Console.ReadLine()!);
+int to = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine();
+
+double[,] matrix = fillDouble2DArray(row, col, from, to);
+printDouble2DArray(matrix);
+System.Console.WriteLine();

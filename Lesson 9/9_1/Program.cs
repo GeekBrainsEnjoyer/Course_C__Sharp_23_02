c303ab8dc5c53986ef1863a3ea8e9094d8eb4118
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-29 18:53:50 +0300

    task 9_1 recursion increased num

---
+++ Lesson 9/9_1/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 1: Задайте значение N. Напишите программу, которая выведет
+// все натуральные числа в промежутке от 1 до N при помощи рекурсии.
+// N = 5 -> "1, 2, 3, 4, 5"
+// N = 6 -> "1, 2, 3, 4, 5, 6"
+
+void SeriesNum(int N)
+{
+    if (N == 0) return;
+    SeriesNum(N - 1);
+    System.Console.WriteLine($"{N} ");
+}
+
+int N = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine();
+SeriesNum(N);

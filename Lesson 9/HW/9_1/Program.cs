8658617a90bb5d77f4c7585d84bc118b2bd98675
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-29 21:13:35 +0300

    hw task 9_1

---
+++ Lesson 9/HW/9_1/Program.cs
@@ -0,0 +1,19 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу, которая выведет все
+// чётные натуральные числа в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 5 -> "2, 4"
+// M = 4; N = 8 -> "4, 6, 8"
+
+void SeriesNumFromTo(int M, int N)
+{
+    if (N == M - 1) return;
+    SeriesNumFromTo(M, N - 1);
+    if (N % 2 == 0)
+        System.Console.Write($"{N} ");
+
+}
+System.Console.Write("M = ");
+int M = int.Parse(Console.ReadLine()!);
+System.Console.Write("N = ");
+int N = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine();
+SeriesNumFromTo(M, N);

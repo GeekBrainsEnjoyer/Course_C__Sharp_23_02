86ce5acd938e3ba4a3ee2a6409514c877768240f
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-29 20:16:40 +0300

    task 9_3

---
+++ Lesson 9/9_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Задача 3: Напишите программу, которая будет принимать на вход число и
+// возвращать сумму его цифр. Использовать рекурсию.
+// 453 -> 12
+// 45 -> 9
+
+int SumElements(int n)
+{
+    if (n == 0) return 0;
+
+    return SumElements(n / 10) + n % 10;
+}
+
+int n = int.Parse(System.Console.ReadLine()!);
+int sum = SumElements(n);
+System.Console.WriteLine(sum);

33ab1a77e93b8d76c0d8e9933da4585753fea6a8
GeekBrainsEnjoyer <rahmatylin.sasha@yandex.ru>
2023-03-29 21:23:17 +0300

    hw task 9_2

---
+++ Lesson 9/HW/9_2/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте значения M и N. Напишите программу, которая найдёт сумму
+// натуральных элементов в промежутке от M до N с помощью рекурсии.
+// M = 1; N = 15 -> 120
+// M = 4; N = 8 -> 30
+
+int SumNumFromMToN(int M, int N)
+{
+
+    if (N == M - 1) return 0;
+
+    return SumNumFromMToN(M, N - 1) + N;
+}
+
+System.Console.Write("M = ");
+int M = int.Parse(Console.ReadLine()!);
+System.Console.Write("N = ");
+int N = int.Parse(Console.ReadLine()!);
+System.Console.WriteLine();
+int sum = SumNumFromMToN(M, N);
+System.Console.WriteLine(sum);
